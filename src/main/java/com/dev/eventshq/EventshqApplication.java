807e0d42acc2d73e00b29dcda022763f1e8af85b
Allan <allanpereira773@gmail.com>
2026-04-22 19:59:43 -0300

    Create and Set up project

---
+++ src/main/java/com/dev/eventshq/EventshqApplication.java
@@ -0,0 +1,13 @@
+package com.dev.eventshq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventshqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventshqApplication.class, args);
+	}
+
+}
